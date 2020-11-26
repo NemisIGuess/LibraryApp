fd8c242af388c9300cc641aa86c6663b583020d8
Nemis <ismaelpjgg@gmail.com>
2020-11-26 13:00:14 +0100

    Change the display from Cards to a Table

--- script.js
+++ script.js
@@ -11,16 +11,36 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-const bookshelf = document.getElementById("container");
+const bookTable = document.getElementById("container");
 function displayBooksInCards() {
     for (let i = 0; i < myLibrary.length; i++) {
-        const bookCard = document.createElement("div");
-        bookCard.classList.add("bookCard");
-        bookshelf.appendChild(bookCard);
+        // const bookCard = document.createElement("div");
+        // bookCard.classList.add("bookCard");
+        // bookTable.appendChild(bookCard); Cambiar a tabla
     }
 }
 
-const addBook = document.getElementById("addBook");
-addBook.addEventListener('click', () => {
-        addBook.classList.toggle("addBookAnimation");
+const addBookButton = document.getElementById("addBookButton");
+addBookButton.addEventListener('click', () => {
+    addBookButton.classList.remove("rotate0")
+    addBookButton.classList.add("rotate45")
+    displayAddBookForm();
 }) 
+
+const body = document.querySelector("body");
+function displayAddBookForm() {
+    body.classList.add("overflow");
+    const viewBlocker = document.createElement("div");
+    viewBlocker.classList.add("viewBlocker");
+    body.appendChild(viewBlocker);
+    viewBlocker.addEventListener("click", () => {
+        addBookButton.classList.toggle("addBookButtonAnimation");
+        body.removeChild(viewBlocker);
+        body.classList.remove("overflow");
+        addBookButton.classList.remove("rotate45")
+        addBookButton.classList.add("rotate0")
+        bookForm.style.visibility = "hidden";
+    });
+    const bookForm = document.getElementById("addBookForm");
+    bookForm.style.visibility = "visible" ;
+}
