d128f2bfd9de28163ef2396b39473b8f959f5799
NemisIGuess <ismaelpjgg@gmail.com>
2020-11-30 19:55:01 +0100

    Added 'Add Book' functionality

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let myLibrary = [];
 
-function Book(title, author,year, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.yearPublished = year;
-    this.read = read;
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
 const bookTable = document.getElementById("container");
-function displayBooksInCards() {
+function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
         // const bookCard = document.createElement("div");
         // bookCard.classList.add("bookCard");
@@ -25,6 +26,7 @@ addBookButton.addEventListener('click', () => {
     addBookButton.classList.remove("rotate0")
     addBookButton.classList.add("rotate45")
     displayAddBookForm();
+    document.getElementById("title").focus();
 }) 
 
 const body = document.querySelector("body");
@@ -33,14 +35,27 @@ function displayAddBookForm() {
     const viewBlocker = document.createElement("div");
     viewBlocker.classList.add("viewBlocker");
     body.appendChild(viewBlocker);
-    viewBlocker.addEventListener("click", () => {
-        addBookButton.classList.toggle("addBookButtonAnimation");
-        body.removeChild(viewBlocker);
-        body.classList.remove("overflow");
-        addBookButton.classList.remove("rotate45")
-        addBookButton.classList.add("rotate0")
-        bookForm.style.visibility = "hidden";
-    });
     const bookForm = document.getElementById("addBookForm");
-    bookForm.style.visibility = "visible" ;
+    bookForm.style.visibility = "visible";
+    viewBlocker.addEventListener("click", clearViewBlocker);
 }
+
+function clearViewBlocker() {
+    const viewBlocker = document.querySelector(".viewBlocker");
+    body.removeChild(viewBlocker);
+    body.classList.remove("overflow");
+    addBookButton.classList.remove("rotate45")
+    addBookButton.classList.add("rotate0")
+    const bookForm = document.getElementById("addBookForm");
+    bookForm.style.visibility = "hidden";
+}
+
+window.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addBookToLibrary(new Book(form.title.value,form.author.value,form.pages.value,form.read.value));
+    form.reset();
+    clearViewBlocker();
+});
+
+
+// MIRAR EL CODIGO DE ZACH INGLE DE COMO MOSTRAR LA TABLA, ETC
