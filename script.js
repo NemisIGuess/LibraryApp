ffce3c1c0f3756b09e0f94c8fe988d3647085cd7
NemisIGuess <ismaelpjgg@gmail.com>
2020-12-02 12:58:34 +0100

    Finished the Library Project

--- script.js
+++ script.js
@@ -8,17 +8,9 @@ class Book {
         this.read = read;
     }
 }
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
-}
-
-const bookTable = document.getElementById("container");
-function displayBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        // const bookCard = document.createElement("div");
-        // bookCard.classList.add("bookCard");
-        // bookTable.appendChild(bookCard); Cambiar a tabla
-    }
 }
 
 const addBookButton = document.getElementById("addBookButton");
@@ -38,6 +30,9 @@ function displayAddBookForm() {
     const bookForm = document.getElementById("addBookForm");
     bookForm.style.visibility = "visible";
     viewBlocker.addEventListener("click", clearViewBlocker);
+    window.addEventListener("keydown", (e) => {
+        if (e.key == 'Escape') {clearViewBlocker()};
+    })
 }
 
 function clearViewBlocker() {
@@ -52,10 +47,79 @@ function clearViewBlocker() {
 
 window.addEventListener('submit', (e) => {
     e.preventDefault()
-    addBookToLibrary(new Book(form.title.value,form.author.value,form.pages.value,form.read.value));
+    addBookToLibrary(new Book(form.title.value, form.author.value, form.pages.value, form.read.value));
     form.reset();
     clearViewBlocker();
+    displayTable();
 });
 
+function removeBook(index) {
+    myLibrary.splice(index, 1)
+}
 
-// MIRAR EL CODIGO DE ZACH INGLE DE COMO MOSTRAR LA TABLA, ETC
+function createRemoveButton(index) {
+    let editTableData = document.createElement('td');
+    let editButton = document.createElement('input');
+    editButton.type = 'image';
+    editButton.src = "/images/bin.png";
+    editButton.addEventListener('click', () => {
+        myLibrary.splice(index, 1);
+        displayTable();
+    });
+    editTableData.appendChild(editButton);
+    return editTableData;
+}
+
+function createReadButton(index) {
+    let readTableData = document.createElement('td');
+    let readButton = document.createElement('input');
+    readButton.type = 'image';
+    readButton.src = "/images/read.png";
+    readButton.addEventListener('click', () => {
+        myLibrary[index].read == "Yes" ? myLibrary[index].read = "No" : myLibrary[index].read = "Yes";
+        displayTable();
+    });
+    readTableData.appendChild(readButton);
+    return readTableData;
+}
+
+function displayTable() {
+    const tableBody = document.querySelector('tbody');
+
+    while (tableBody.firstChild) {
+        tableBody.removeChild(tableBody.firstChild);
+    }
+
+    myLibrary.forEach((book, index) => {
+        let tableRow = document.createElement('tr');
+        Object.keys(book).forEach(prop => {
+            let tableData = document.createElement('td');
+            tableData.textContent = book[prop];
+            tableRow.appendChild(tableData);
+    })
+    
+    tableRow.appendChild(createReadButton(index));
+    tableRow.appendChild(createRemoveButton(index));
+    tableBody.appendChild(tableRow);
+    })
+
+    saveDataToLocal();
+}
+
+function saveDataToLocal() {
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+}
+
+function getDataFromLocal() {
+    myLibrary = JSON.parse(localStorage.getItem('library'));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (!localStorage.getItem('library')) {
+        saveDataToLocal();
+    } else {
+        getDataFromLocal();
+    }
+
+    displayTable();
+})
